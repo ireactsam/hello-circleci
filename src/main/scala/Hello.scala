7ce5f2960dc4d61c3f0cf516913c26cff0d6c030
sdebacker <sam.de.backer@persgroep.be>
2017-10-27 16:13:56 +0200

    circleci .

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -3,4 +3,5 @@ import io.ireact.BuildInfo
 object Hello extends App {
 
   println(s"Hello from ${BuildInfo.name} ${BuildInfo.version} build at ${BuildInfo.builtAtString} UTC.")
+  println("Go home")
 }
